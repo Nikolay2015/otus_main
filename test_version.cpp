91f09f58f1e07ba075a576acb49dab803f6fcf8d
Nikolay2015 <mushroom8613@gmail.com>
2023-01-08 01:30:15 +0300

    fix cmake and google test

--- test_version.cpp
+++ test_version.cpp
@@ -2,13 +2,26 @@
 
 #include "lib.h"
 
-#include <boost/test/unit_test.hpp>
+//#include <boost/test/unit_test.hpp>
+#include <gtest/gtest.h>
 
-BOOST_AUTO_TEST_SUITE(test_version)
+//BOOST_AUTO_TEST_SUITE(test_version)
 
-BOOST_AUTO_TEST_CASE(test_valid_version)
-{
-    BOOST_CHECK(version() > 0);
+//BOOST_AUTO_TEST_CASE(test_valid_version)
+//{
+//    BOOST_CHECK(version() > 0);
+//}
+
+//BOOST_AUTO_TEST_SUITE_END()
+
+TEST(check_version, test_valid_version_project) {
+    EXPECT_TRUE(version() > 0);
 }
 
-BOOST_AUTO_TEST_SUITE_END()
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+
